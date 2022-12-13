a8fecc82cd904d8e73fa043bbce984c5ec858042
UbuntuKosuke <Ubuntu.Kosuke@gmail.com>
2022-12-14 05:49:51 +0900

    35.StaticFilesの設定

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"))
 
 
 @app.get("/", response_class=HTMLResponse)
