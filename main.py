94d5112f2de3898b2b1ecd3ea26f078306bb58c2
UbuntuKosuke <Ubuntu.Kosuke@gmail.com>
2022-12-14 05:02:36 +0900

    33.FastAPIを用いたHTMLのHalloWorld

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 async def root():
-    return {"message": "Hello World"}
+    with open("index.html","r") as f:
+        return f.read()
 
