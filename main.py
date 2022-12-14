d6a9bf8be18e37484d8be724d59a617083a7074e
UbuntuKosuke <Ubuntu.Kosuke@gmail.com>
2022-12-14 10:14:53 +0900

    41.ONNX Runtimeを使った推論処理の実装

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from io import BytesIO
 from fastapi import File, UploadFile
 from PIL import Image
 
+import numpy
+import onnxruntime
+
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"))
@@ -20,8 +23,19 @@ async def root():
 async def predict(image: UploadFile = File(...)):
     image_data = await image.read()
     pil_image = Image.open(BytesIO(image_data))
-    pil_image.save('number.png')
 
-    probabilities = [ 0.2, 0.3, 0.5, 0, 0, 0, 0, 0, 0, 0 ]
-    return {'probabilities': probabilities}
+    # preprocess
+    resized_image = pil_image.resize((28, 28))
+    resized_arr = numpy.array(resized_image)
+    transposed_arr = resized_arr.transpose(2,0,1)
+    alpha_arr = transposed_arr[3]
+    reshaped_arr = alpha_arr.reshape(-1)
+    input = [reshaped_arr.astype(numpy.float32)]
+
+    # predict
+    onnx_session = onnxruntime.InferenceSession("model.onxx")
+    output = onnx_session.run(['probabilities'],{'float_input': input})
+    result = output[0][0]
+
+    return {'probabilities': result.tolist()}
 
