ea546a1493912eaa13caa07e2b731cf27d340202
UbuntuKosuke <Ubuntu.Kosuke@gmail.com>
2022-12-14 09:17:52 +0900

    39.推論結果を画面に表示

--- static/main.js
+++ static/main.js
@@ -13,4 +13,33 @@ predictButtonElement.addEventListener("click", async() => {
   const response = await fetch('/api/predict')
   const responseData = await response.json()
   console.log(responseData);
+  //
+  // 推論結果をtbodyに表示
+  const tablebodyElement = document.getElementById('result-table-body')
+
+  while(tablebodyElement.firstChild){
+    tablebodyElement.removeChild(tablebodyElement.firstChild)
+  }
+
+  const probabilities = responseData.probabilities
+  let count = 0
+  probabilities.forEach(element => {
+    const tr = document.createElement('tr')
+
+    // 数字
+    const tdNumber = document.createElement('td')
+    tdNumber.textContent = count
+    tr.appendChild(tdNumber)
+
+    // 確率
+    const tdProbability = document.createElement('td')
+    tdProbability.textContent = (element * 100).toFixed(1)
+    tr.appendChild(tdProbability)
+
+    tablebodyElement.appendChild(tr)
+    count++
+  });
+
 })
+
+
